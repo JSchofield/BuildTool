1745e585cd187ed46e1727bbbffdc8443bdb9708
Jonathon Schofield <jonathon.schofield@gmail.com>
2011-11-06 15:47:22 +0000

    Removed old file

--- src/BuildTool/ProcessInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BuildTool
-{
-    public class ProcessInfo
-    {
-        public string FileName { get; set; }
-        public string Arguments { get; set; }
-        public string WorkingDirectory { get; set; }
-    }
-}
